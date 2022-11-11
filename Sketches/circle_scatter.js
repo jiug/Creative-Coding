15832c8518c5f7a0c523970f1a092696d235d8e7
jiug <hbeltranh@protonmail.com>
2022-11-11 15:39:16 +0100

    Done the work for today

--- Sketches/circle_scatter.js
+++ Sketches/circle_scatter.js
@@ -14,43 +14,42 @@ const sketch = () => {
     context.fillStyle = "black";
 
     let x = 140;
-    let y = 990;
+    let y = 100;
 
     const num = 200;
 
     let noise = [];
 
-    const drift = 0.05;
+    const drift = 0.015;
 
     for (let i = 0; i < num; i++) {
-        const slice = math.degToRad(360/num);
+        let count = 0;
 
-        x += 0;
-        y -= (4 -drift);
+        x += 1.8;
+        y += (5-0.01*i);
 
-        
+        // Populate a different perlin noise array for every line
+        for (l = 0; l < 200; l++){
+            noise[l] = random.noise2D(drift*l,drift*i);
+        }
 
         context.save();
         context.translate(x,y);
 
         context.beginPath();
-        context.moveTo(0,0);
 
-        for (l = 0; l < num; l++){
-            noise[l] = random.noise2D(0.015*l,0.02*i);
-        }
+        // Draw every line point by point
+        for (j = 0; j < noise.length - i; j++) {
+            let a = i;
 
-        let count = 0;
-
-        for (j = 0; j < noise.length; j++) {
-
-            let a = i
-            context.lineTo(4*count+1,0.5*a*noise[j]);
+            context.lineTo(4*count,0.5*a*noise[j]);
             count++;
         };
 
         context.stroke();
         context.restore();
+
+
 
     };
   };
